7b90f8405d6a8c66b9fb0595ae346df8cd80402e
sudharshan2989 <sudmeetup@gmail.com>
2026-05-30 17:38:42 +0530

    Updated commit

--- hosting/frontend_hosting.py
+++ hosting/frontend_hosting.py
@@ -4,8 +4,9 @@ import os
 
 HF_TOKEN = os.getenv("HF_TOKEN")
 api = HfApi(token=os.getenv("HF_TOKEN"))
+
 api.upload_folder(
-    folder_path="predictive-vehicle-maintenance/deployment/frontend_files",     # the local folder containing your files
+    folder_path="deployment/frontend_files",     # the local folder containing your files
     repo_id="sudharshanc/predictive-vehicle-maintenance-frontend",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
